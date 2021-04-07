4bf9e119b7532ed11ef8646329b8a93944202f23
Elco Garcia <garciaelco18@gmail.com>
2021-04-07 11:32:46 -0500

    added sessions to routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const session = require('express-session')
 
 const errorController = require('./controllers/error');
 
@@ -19,6 +20,7 @@ app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret:'my secret', resave:false, saveUninitialized: false}))
 
 app.use((req, res, next) => {
   User.findById("606237068a1ab15eea9ae0a9")
