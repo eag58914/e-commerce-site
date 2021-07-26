02032c712616907d9eebf4e97bab074a5b2e7815
Elco Garcia <garciaelco18@gmail.com>
2021-07-26 13:56:37 -0500

    initial deployment

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ const store = new MongoDBStore({
 })
 const csrfProtection = csrf();
 
-const privateKey = fs.readFileSync('server.key')
-const certificate = fs.readFileSync('server.cert')
+//const privateKey = fs.readFileSync('server.key')
+//const certificate = fs.readFileSync('server.cert')
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
