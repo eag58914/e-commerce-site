7c2d41da8c3eec49ec42136936225031d127a533
eag58194 <garciaelco18@gmail.com>
2022-11-07 11:57:46 -0600

    admin authentication issue

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,18 @@
 module.exports = (req,res,next)=>{
+
     if(!req.session.isLoggedIn){
         return res.redirect('/login')
     }
     next()
 }   
+
+// module.exports = (roles)=>{
+//     return (req,res,next) =>{
+//         if(req.session.isLoggedIn){
+//             if(roles.includes(req.user.role)){
+//                 return next()
+//             }
+//         }
+//         return res.redirect('/login')
+//     }
+// }
