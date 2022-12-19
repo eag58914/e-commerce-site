f0b19e24a0d072f857ec85ed88dffc16a6034eb1
eag58194 <garciaelco18@gmail.com>
2022-12-19 16:04:24 -0600

    Added NavBar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,14 @@
 import axios from 'axios'
 
 import React, { useState, useEffect } from 'react'
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import ProductPage from './components/ProductPage';
+import ResponsiveAppBar from './components/NavBar';
 
 const baseurl = "http://localhost:3000/products"
 
 function App() {
-  const [data, setData] = useState()
+  const [data, setData] = useState([{"id":"123245","title":"A Book","imageUrl":"https://www.publicdomainpictures.net/pictures/10000/velka/1-1210009435EGmE.jpg","description":"This is an awesome book!","price":"19"},{"id":"0.41607315815753076","title":"fasfd","imageUrl":"fdasfs","description":"fadsfads","price":"12"}])
 
 
   useEffect(()=>{
@@ -16,18 +18,23 @@ function App() {
   })
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-  <ul>
-{data.map((element, index)=>{
-return(
-  <li key={index}>{element.title} </li>
-)
-})}
-</ul>
+  return(
+  <div>
+    <header className="App-header"></header>
+    <ResponsiveAppBar />     
+      <ul>
+      {data.map((element, index)=>
+        <li key={index}>{element.title} {element.price}{element.imageUrl} </li>)}
+        </ul>
+<BrowserRouter>
+<Routes>
+  <Route  exact path="/products"  element={<ProductPage/>}/>
+</Routes>
+</BrowserRouter>
+
+
      
-      </header>
+      
     </div>
   );
 }
